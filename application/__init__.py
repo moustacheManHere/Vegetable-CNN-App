fe174fe1a7664a1fadc6888f8c0afcaaef5b2b5d
ram <justforthegreatergood@gmail.com>
2024-02-09 16:49:47 +0000

    Closes #26, connected to API

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
 app.config["UPLOADS_DEFAULT_DEST"] = "application/static/"
 app.config["UPLOADED_PHOTOS_DEST"] = "uploads"
+app.config["IMAGE_LOCATION"] = "static/uploads"
 
 photos = UploadSet("uploads", IMAGES)
 configure_uploads(app, photos)
--- application/routes.py
+++ application/routes.py
@@ -3,6 +3,26 @@ from flask import render_template, request, flash, send_from_directory, url_for
 from flask import json, jsonify
 from application.forms import UploadForm
 from application import photos
+from application.deeplearning import get_prediction
+
+
+vegetable_list = [
+    "Bean",
+    "Bitter_Gourd",
+    "Bottle_Gourd",
+    "Brinjal",
+    "Broccoli",
+    "Cabbage",
+    "Capsicum",
+    "Carrot",
+    "Cauliflower",
+    "Cucumber",
+    "Papaya",
+    "Potato",
+    "Pumpkin",
+    "Radish",
+    "Tomato"
+]
 
 @app.route('/')
 @app.route('/index')
@@ -17,13 +37,15 @@ def predict():
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
         file_url = url_for("get_file", filename= filename)
+        response = get_prediction(filename, 31) # later do dynamic size switching function
     else:
         file_url = None
-    return render_template("predict.html", form = form, file_url=file_url)
+        response = None
+    return render_template("predict.html", form = form, file_url=file_url, response = response, vegetable_list=vegetable_list) # try to have a more human response. 
 
 @app.route('/uploads/<filename>')
 def get_file(filename):
-    return send_from_directory("static/uploads", filename)
+    return send_from_directory(app.config["IMAGE_LOCATION"], filename)
 
 
 #@app.errorhandler(Exception)
