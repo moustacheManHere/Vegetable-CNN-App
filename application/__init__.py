c6a875b15bc812d3d0c8e84b81e3994561483de8
ram <justforthegreatergood@gmail.com>
2024-02-10 15:02:04 +0000

    Closes #35, created database for vege catalogue

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy()
+
+with app.app_context():
+    db.init_app(app)
+
+    from .models import *
+
+    db.create_all()
+    populate_vege()
+    db.session.commit()
+    print("Created Database!")
 
 from application import routes
