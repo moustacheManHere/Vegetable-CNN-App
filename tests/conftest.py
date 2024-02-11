21885bf76cd0174892e6aace31db7e3a0a095e6f
ram <justforthegreatergood@gmail.com>
2024-02-11 05:09:49 +0000

    Related to #38, testing and rollback fully works

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from application import app as application
+from application import db as database
 import os 
 import io
 
@@ -17,3 +18,10 @@ def test_image():
     image_path = "application/static/images/0001.jpg"
     with open(image_path, 'rb') as f:
         return io.BytesIO(f.read())
+
+@pytest.fixture
+def db(app):
+    with app.app_context():
+        database.create_all()
+        yield database
+        database.drop_all()
