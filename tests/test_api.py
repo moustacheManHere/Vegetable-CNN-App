d37be94fecbfa7eef73777363184e3127116d9e7
ram <justforthegreatergood@gmail.com>
2024-02-11 05:50:49 +0000

    Closes #43, done with testing for veges

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,3 +12,15 @@ def test_predict_page_get(client):
 def test_error_handler(client):
     response = client.get('/lmao')
     assert response.status_code == 404
+
+def test_veges_page_get(client):
+    response = client.get('/veges')
+    assert response.status_code == 200
+    assert b"Tommy's Friends" in response.data
+    assert b"Bitter_Gourd" in response.data
+    
+@pytest.mark.parametrize("vegetable_id", list(range(1, 16)))
+def test_info_page_get(client, vegetable_id):
+    response = client.get(f'/info/{vegetable_id}')
+    assert response.status_code == 200
+    assert b"Vegetable Details" in response.data
--- tests/test_expected.py
+++ tests/test_expected.py
@@ -30,3 +30,10 @@ def test_missing_input(client,app, test_image):
     soup = BeautifulSoup(response.data, 'html.parser')
     result_message = soup.find('h3', class_='mt-3').get_text(strip=True)
     assert "I'm" in result_message
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("vegetable_id", [0,16]) # test boundary cases
+def test_info_page_wrong(client, vegetable_id):
+    response = client.get(f'/info/{vegetable_id}')
+    assert response.status_code == 200
+    assert b"Vegetable Details" in response.data
